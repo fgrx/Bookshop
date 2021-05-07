724b78e37c248fe71fea5acf6d2c6cac8a29011d
Fabien <fabien.grignoux@outlook.com>
2021-05-07 18:27:55 +0200

    Nodemon params

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export class BookController {
   public getAllBooks(req: Request, res: Response) {
     res.json({
-      message: "Hello World",
+      message: "Hello World 2",
     });
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
+import { Request, Response } from "express";
 import { BookController } from "../controllers/bookController";
 
 export class Routes {
   public bookController = new BookController();
 
   public routes(app: any): void {
-    app.route("/books").get(this.bookController.getAllBooks);
+    //app.route("/books").get(this.bookController.getAllBooks);
+
+    app.route("/books").get((req: Request, res: Response) => {
+      this.bookController.getAllBooks(req, res);
+    });
   }
 }
